29953cbc0edf13a030e191457cfad420b5a835b1
0x233 <76194351+Anzai0x@users.noreply.github.com>
2021-06-22 16:29:54 +0800

    Update meson.sh

--- meson.sh
+++ meson.sh
@@ -3,6 +3,10 @@
 firewall-cmd --zone=public --add-port=19091/tcp --permanent
 firewall-cmd --reload
 
+#安装screen
+yum update
+yum install -y screen
+
 #下载解压
 wget 'https://coldcdn.com/api/cdn/f2cobx/terminal/v2.5.1/meson-linux-amd64.tar.gz'
 tar -zxf meson-linux-amd64.tar.gz
